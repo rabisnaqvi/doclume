962d3d6d23486b311a84feb1983eab8bfb5cebfc
Rabis Naqvi <rabis.naqvi@emplifi.io>
2026-05-19 20:08:46 +0200

    feat: add self-hosted variable fonts and update testing workflow

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { bootstrapRoot } from '@doclume/core';
 import { App } from './App';
+import './fonts.css';
 import './app.css';
 
 bootstrapRoot((root) => {
